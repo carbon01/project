f996ad060e6386f03801b4eb067d4800566cc89a
carbon01 <59917786+carbon01@users.noreply.github.com>
2022-05-26 10:37:06 +0530

    Update order.js

--- order.js
+++ order.js
@@ -1,24 +1,22 @@
-function reefreshOrderStatus(orderStatusCard) {
-    var orderCancelButton = getCancelButton(orderStatusCard);
-    var orderNo = parseInt(orderStatusCard.find('.card-title').text().substring(orderNumberOffset))
-    var status = arrayCounter[orderNo]["currentStatus"]
+function viewOrderStatus(orderStatusCard) {
+    var orderCancelButton = getCancelButtonElement(orderStatusCard);
+    var orderNo = parseInt(orderStatusCard.find('.card-title').text().substring(orderNumberOffset));
+    var status = getCurrentOrderStatus(orderNo); //Enum Status
     
-    showCurrentOrderList(orderNo); // Displays current order is side bar
     reflectOrderCardActive(orderNo); // Changes the state of order card to show the customer
                                      // the card for which the status is being displayed.
-    
-    if (isStatusNonCancellable(status)) { // If preparation of this order has started, 
-                                          // access for the customer to cancel the order will be removed.
-        removeCancelButton(orderCancelButton);
-    }
 
-    if (hasOrderStatusReachedFinal(status)) { // If Delivered (or) Cancelled
+    if (isOrderStatusFinal(status)) { // If Delivered (or) Cancelled
         changeCardBody(orderStatusCard, status);
-        removeTableBody();
     } else {
-        refreshOrderNumber(orderNo);
-        refreshTableBody();
-        blinkOrderStatus(status); // Blinks the current order status
+        if (isOrderNonCancellable(status)) {        // If preparation of this order has started, 
+            removeCancelButton(orderCancelButton);  // access for the customer to cancel the order will be removed.
+        }
+        refreshOrderNumberText(orderNo);
+        displayOrderStatusTable();
+        blinkCurrentOrderStatus(status);
     }
+    
+    showCurrentOrderList(orderNo, status); // Displays current order in side bar, if order not Cancelled
 
 }
